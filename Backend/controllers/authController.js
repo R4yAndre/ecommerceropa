81b9032d137ec911d867b727ee8b0969942b5098
Ray Davalos <rayandredavalostp@gmail.com>
2025-07-22 20:06:25 -0500

    Clave almacenada ahora en .env

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,8 +1,9 @@
+require('dotenv').config(); // al inicio si no está aún
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const Usuario = require('../models/Usuario');
 
-const SECRET_KEY = '07aa163743a9681c02eeeca106e2ebb1abe556e11ab2046c2662c8f6326f63bf';
+const SECRET_KEY = process.env.JWT_SECRET;
 
 const login = async (req, res) => {
   try {
@@ -14,9 +15,11 @@ const login = async (req, res) => {
     const contraseñaValida = await bcrypt.compare(contraseña, usuario.contraseña);
     if (!contraseñaValida) return res.status(401).json({ error: 'Contraseña incorrecta' });
 
-    const token = jwt.sign({ id: usuario.id, email: usuario.email }, SECRET_KEY, {
-      expiresIn: '2h'
-    });
+    const token = jwt.sign(
+      { id: usuario.id, email: usuario.email },
+      SECRET_KEY,
+      { expiresIn: '2h' }
+    );
 
     res.json({ token });
   } catch (error) {
